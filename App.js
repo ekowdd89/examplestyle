f897fb82899dd0b1f47c8f8f619c8f05e90c0682
umrohku team <umrohku@umrohkus-MacBook-Pro.local>
2020-06-19 13:34:59 +0700

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {styles} from "./Styles";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txt}>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Styles.js
@@ -0,0 +1,18 @@
+import React, { Comoponents } from "react";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'red',
+        justifyContent:'center',
+        alignContent:'center',
+        alignItems:'center'
+    },
+    /// change text color
+    txt:{
+        color:'#fff',
+        fontSize:20,
+        fontWeight:'bold'
+    }
+})
